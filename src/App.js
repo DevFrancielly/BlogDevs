e881dda0ab38c919b8abca6894b929b3b9d09a01
Francielly Silva <devfrancielly@gmail.com>
2025-01-28 11:10:22 -0300

    :sparkles: feat: Creating routes between pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import "./components/HomePage/index"
 import "./components/AboutMe/index"
 import HomePage from "./components/HomePage/index";
 import AboutMe from "./components/AboutMe/index";
+import { BrowserRouter } from "react-router-dom";
 
-const page = window.location.pathname === '/' ? <HomePage/> : <AboutMe/>
+// const page = window.location.pathname === '/' ? <HomePage/> : <AboutMe/>
 
 function App() {
-  return page;
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/SobreMim" element={<AboutMe/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
